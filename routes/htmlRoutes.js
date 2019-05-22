3a2a664451216819b68aaf13044bae419311ad77
JDC-RVA <jeremy@firstmotionlc.com>
2019-05-21 21:14:10 -0400

    generated news articles from API

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,19 +1,31 @@
 var db = require("../models");
+const NewsAPI = require("newsapi");
+const newsapi = new NewsAPI("f040e9c3149442ae93a7d68d6e99271b");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
+    newsapi.v2
+      .topHeadlines({
+        // sources: "bbc-news,the-verge",
+        q: "bitcoin",
+        category: "business",
+        language: "en"
+        // country: "us"
+      })
+      .then(response => {
+        // console.log(response);
+        res.render("index", {
+          newsArticles: response.articles
+        });
       });
-    });
   });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.render("example", {
         example: dbExample
       });
