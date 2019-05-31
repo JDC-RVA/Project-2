822f9cf09e79db90ef656f5442604fa0d1e3d887
JDC-RVA <jeremy@firstmotionlc.com>
2019-05-30 21:06:56 -0400

    added entertainment and sports html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,13 +3,13 @@ const NewsAPI = require("newsapi");
 const newsapi = new NewsAPI("f040e9c3149442ae93a7d68d6e99271b");
 
 module.exports = function(app) {
-  // Load index page
+  // Load index page displaying top news
   app.get("/", function(req, res) {
     newsapi.v2
       .topHeadlines({
         // sources: "bbc-news,the-verge",
         q: "",
-        category: "business",
+        category: "",
         // language: "en"
         country: "us",
         pageSize: 100
@@ -23,7 +23,49 @@ module.exports = function(app) {
       });
   });
 
-  // Load example page and pass in an example by id
+  // Load entertainment page
+  app.get("/entertainment", function(req, res) {
+    console.log(Date.now());
+    newsapi.v2
+      .everything({
+        // q: req.params.search,
+        category: "entertainment",
+        from: "2019-12-01",
+        to: "2017-12-12",
+        language: "en",
+        pageSize: 100
+        // country: "us"
+      })
+      .then(response => {
+        // console.log(response);
+        res.render("index", {
+          newsArticles: response.articles
+        });
+      });
+  });
+
+  // Load sports page
+  app.get("/sports", function(req, res) {
+    console.log(Date.now());
+    newsapi.v2
+      .everything({
+        // q: req.params.search,
+        category: "sports",
+        from: "2019-12-01",
+        to: "2017-12-12",
+        language: "en",
+        pageSize: 100
+        // country: "us"
+      })
+      .then(response => {
+        // console.log(response);
+        res.render("index", {
+          newsArticles: response.articles
+        });
+      });
+  });
+
+  // Load user search page
   app.get("/:search", function(req, res) {
     console.log(Date.now());
     newsapi.v2
